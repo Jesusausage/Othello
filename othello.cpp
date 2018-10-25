2766c84695aa2b5035ba3c9cc68a911ddfc96d97
Philip Cai <philipcai@hotmail.co.uk>
2018-10-25 15:14:41 +0100

    Tidied up style, fixed piece-flipping

--- othello.cpp
+++ othello.cpp
@@ -5,15 +5,21 @@ using namespace std;
 
 class Piece {
 public:
-    void place_piece(char player); /* Sets the piece's colour to 'player'. */
-    void set_neighbours(Piece board[8][8], int row, int col, Piece null);
-    char get_colour() const;
-    Piece get_neighbour(int dir) const;
-    void get_neighbours(Piece *neighbours[8]) const; /* Assigns the piece's neighbour pointers to
-						       the array of pointers '*neighbours[8]'. */  
+    void placePiece(char player);
+    void setNeighbours(Piece board[8][8], int row, int col, Piece null);
+    void setLocation(int row, int col);
+    /* Set a piece's colour, neighbours and coordinates. */
+    
+    char colour() const;
+    Piece neighbour(int dir) const;
+    int row() const;
+    int col() const;
+    /* Simple functions which return colour_, *neighbour_[dir] (dereferenced), row_ and col_. */
+ 
 private:
-    char colour;   
-    Piece *neighbour[8]; /* An array of pointers 
+    int row_, col_;
+    char colour_;
+    Piece *neighbour_[8]; /* An array of pointers 
 			    to each of the piece's eight surrounding neighbours, where
 			    neighbour[0] is directly above, then they progress anticlockwise. */
 };
@@ -21,37 +27,48 @@ private:
 
 class Move {
 public:
-    void set_score(float score_value);
-    void set_player(char colour);
-    void set_location(int row_no, int col_no);
-    float get_score() const;
-    char get_player() const;
-    int get_row() const;
-    int get_col() const;    
+    void setScore(float score);
+    void setPlayer(char colour);
+    void setLocation(int row, int col);
+    /* Set a move's score, player and coordinates. */
+    
+    float score() const;
+    char player() const;
+    int row() const;
+    int col() const;
+    /* Return score_, player_, row_ and col_. */
+    
 private:
-    int row;
-    int col;
-    char player;
-    float score; /* higher means better chance of winning */
+    int row_, col_;
+    char player_;
+    float score_; /* higher means better chance of winning */
 };
     
 
-void print_board(const Piece board[8][8]);
+void printBoard(const Piece board[8][8]);
 
-void player_move(Piece board[8][8], char &player);
+/* Prompt human player move. */
+void playerMove(Piece board[8][8], char &player);
 
-bool check_legal_move(const Piece board[8][8], const Move move, int &dir, Piece &end_of_line);
+bool checkLegalMove(const Piece board[8][8], const Move move);
 
 /* Returns 'W' for 'B', and vice versa. */
 char opponent(char player);
 
-/* Detects whether an enemy piece is followed by an unbroken line of enemy pieces, followed by
-   a friendly piece. If true, the function returns the address of the friendly piece, and 
-   NULL otherwise.
-   The enemy piece is 'piece', and 'dir' is its position relative to the attempted move. */
-bool check_line(Piece piece, int dir, char player, Piece &end_of_line);
+/* Detection for an unbroken line of opponent pieces, 
+   immediately followed by a friendly piece (in direction 'dir'). */
+bool checkLine(Piece piece, int dir, char player);
 
-void swap_player(char &player);
+void swapPlayer(char &player);
+
+/* Sets the initial board state, with out-of-bounds neighbours set to null. */
+void setBoard(Piece board[8][8], Piece null);
+
+/* Flip the required lines of pieces after a legal move. */
+void transformBoard(Piece board[8][8], Move move);
+
+/* Flip a single line of opponent's pieces in direction 'dir'  after a legal move. */
+bool transformLine(Piece piece, int dir, char player, Piece board[8][8]);
 
 
 int main()
@@ -60,182 +77,168 @@ int main()
     char player = 'W';
     Piece null; /* The 'null' piece neighbours edge pieces which would otherwise have 
 		   out-of-bounds neighbours. */
-    null.place_piece(' ');
+    null.placePiece(' ');
 
-    /* Setting the board. */
-    for (int rows=0; rows<8; rows++) {
-	for(int cols=0; cols<8; cols++) {
-	    board[rows][cols].place_piece(' ');
-	    board[rows][cols].set_neighbours(board, rows, cols, null);
-	}
-    }
-    board[3][3].place_piece('W');
-    board[3][4].place_piece('B');
-    board[4][3].place_piece('B');
-    board[4][4].place_piece('W');
+    setBoard(board, null);
 
-    print_board(board);
+    printBoard(board);
     while (1) {
-	player_move(board, player);
-	print_board(board);
+	playerMove(board, player);
+	printBoard(board);
     }
     
     return 0;
 }
 
 
-void Piece::place_piece(const char player) {
-    colour = player; }
+void Piece::placePiece(const char player) {
+    colour_ = player; }
 
-char Piece::get_colour() const {
-    return colour; }
+void Piece::setLocation(int row, int col) {
+    row_ = row; col_ = col; }
 
-Piece Piece::get_neighbour(int dir) const {
-    return *neighbour[dir]; }
-
-void Piece::set_neighbours(Piece board[8][8], int row, int col, Piece null)
+void Piece::setNeighbours(Piece board[8][8], int row, int col, Piece null)
 {
     if (row >= 1)
-	neighbour[0] = &board[row-1][col];
-    else neighbour[0] = &null;
+	neighbour_[0] = &board[row-1][col];
+    else neighbour_[0] = &null;
     
     if (row>=1 && col>=1)
-	neighbour[1] = &board[row-1][col-1];
-    else neighbour[1] = &null;
+	neighbour_[1] = &board[row-1][col-1];
+    else neighbour_[1] = &null;
     
     if (col >= 1)
-	neighbour[2] = &board[row][col-1];
-    else neighbour[2] = &null;
+	neighbour_[2] = &board[row][col-1];
+    else neighbour_[2] = &null;
     
     if (row<=6 && col>=1)
-	neighbour[3] = &board[row+1][col-1];
-    else neighbour[3] = &null;
+	neighbour_[3] = &board[row+1][col-1];
+    else neighbour_[3] = &null;
     	     
     if (row <= 6)
-	neighbour[4] = &board[row+1][col];
-    else neighbour[4] = &null;
+	neighbour_[4] = &board[row+1][col];
+    else neighbour_[4] = &null;
     
     if (row<=6 && col<=6)
-	neighbour[5] = &board[row+1][col+1];
-    else neighbour[5] = &null;
+	neighbour_[5] = &board[row+1][col+1];
+    else neighbour_[5] = &null;
     
     if (col <= 6)
-	neighbour[6] = &board[row][col+1];
-    else neighbour[6] = &null;
+	neighbour_[6] = &board[row][col+1];
+    else neighbour_[6] = &null;
     
     if (row>=1 && col<=6)
-	neighbour[7] = &board[row-1][col+1];
-    else neighbour[7] = &null;
+	neighbour_[7] = &board[row-1][col+1];
+    else neighbour_[7] = &null;
+    /* A piece's neighbours are set to the surrounding eight pieces, unless they are 
+       out of bounds, in which case the neighbour is set to the null piece. */
 }
 
-void Piece::get_neighbours(Piece *neighbours[8]) const {
-    for (int i=0; i<8; i++)
-	neighbours[i] = neighbour[i];
-}
+char Piece::colour() const {
+    return colour_; }
+
+Piece Piece::neighbour(int dir) const {
+    return *neighbour_[dir]; }
+
+int Piece::row() const {
+    return row_; }
+
+int Piece::col() const {
+    return col_; }
 
 
-void Move::set_score(float score_value) {
-    score = score_value; }
+void Move::setScore(float score) {
+    score_ = score; }
 
-void Move::set_location(int row_no, int col_no) {
-    row = row_no; col = col_no; }
+void Move::setLocation(int row, int col) {
+    row_ = row; col_ = col; }
 
-void Move::set_player(char colour) {
-    player = colour; }
+void Move::setPlayer(char colour) {
+    player_ = colour; }
 
-float Move::get_score() const {
-    return score; }
+float Move::score() const {
+    return score_; }
 
-int Move::get_row() const {
-    return row; }
+int Move::row() const {
+    return row_; }
 
-int Move::get_col() const {
-    return col; }
+int Move::col() const {
+    return col_; }
 
-char Move:: get_player() const {
-    return player; }
+char Move:: player() const {
+    return player_; }
 
 
-void print_board(const Piece board[8][8])
+void printBoard(const Piece board[8][8])
 {
     cout << "\n   ";
     for (int i=1; i<=8; i++)
 	cout << "  " << i << " ";
     cout << endl;
+    /* Prints the column labels. */
     
     for (int rows=0; rows<8; rows++) {
 	cout << "   ";
 	for (int i=0; i<8; i++)
-	    cout << "+===";
+	    cout << "+---";
 	char row = rows + 'A';
 	cout << "+\n " << row << " |";
 
 	for (int cols=0; cols<8; cols++)
-	    cout << " " << board[rows][cols].get_colour() << " |";	
+	    cout << " " << board[rows][cols].colour() << " |";	
 	cout << "\n";
     }
+    /* Prints a horizontal board line, then each row and their contents. */
 	
-	cout << "   ";
-	for (int i=0; i<8; i++)
-	    cout << "+===";
-	cout << "+\n\n";	    
+    cout << "   ";
+    for (int i=0; i<8; i++)
+	cout << "+---";
+    cout << "+\n\n";
+    /* Prints the bottom line of the board. */
 }
 
 
-void player_move(Piece board[8][8], char &player)
+void playerMove(Piece board[8][8], char &player)
 {
     char tile[2];
     Move move;
-    int dir;
-    Piece end_of_line;
 
-    move.set_player(player);
-
+    move.setPlayer(player);
     cout << player << "'s move. Your turn.\n";
     cout << "Enter coordinates to make a move: ";
     cin >> tile;
-    move.set_location(tile[0]-'A', tile[1]-'1');
+    move.setLocation(tile[0]-'A', tile[1]-'1');
     
-    while (!check_legal_move(board, move, dir, end_of_line)) {
+    while (!checkLegalMove(board, move)) {
 	cout << "Illegal move. Please enter another location: ";
 	cin >> tile;
-	move.set_location(tile[0]-'A', tile[1]-'1');
+	move.setLocation(tile[0]-'A', tile[1]-'1');
     }
+    /* Continue asking for a move until a legal position is chosen. */
 
-    board[ move.get_row() ][ move.get_col() ].place_piece(player);
-
-    Piece start = board[ move.get_row() ][ move.get_col() ].get_neighbour(dir);
-    while(end_of_line.get_neighbour((dir+4)%8).get_colour() != player) {
-	; // AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-    }
-    
-    swap_player(player);
+    transformBoard(board, move);    
+    swapPlayer(player);
+    /* Places the required pieces on the board, then swaps player. */
 }
    
 
-
-bool check_legal_move(const Piece board[8][8], const Move move, int &dir, Piece &end_of_line)
+bool checkLegalMove(const Piece board[8][8], const Move move)
 {
-    /* Gets the coordinates, player, and neighbours of the given move and stores them locally. */
-    int row = move.get_row();
-    int col = move.get_col();
-    char player = move.get_player();
-    Piece *neighbour[8];
-    board[row][col].get_neighbours(neighbour);
-
-    if (row<0 || row>7 || col<0 || col>7)
+    if (move.row()<0 || move.row()>7 || move.col()<0 || move.col()>7)
 	return 0;
-    if (board[row][col].get_colour() != ' ')
+    if (board[move.row()][move.col()].colour() != ' ')
 	return 0;
+    /* Basic out of bounds check and empty-tile check first. */
 
-    /* In each direction, checks if there is a neighbouring enemy piece, and if there is,
-       checks if it is succeeded by an unbroken line of them, followed by a friendly piece. */
-    for (dir=0; dir<8; dir++) {
-	if ( (*neighbour[dir]).get_colour() == opponent(player) ) {
-	    if (check_line(*neighbour[dir], dir, player, end_of_line))
+    for (int dir=0; dir<8; dir++) {
+	if ( board[move.row()][move.col()].neighbour(dir).colour() == opponent(move.player()) ) {
+	    if ( checkLine(board[move.row()][move.col()].neighbour(dir), dir, move.player()) )
 		return 1;
 	}
     }
+    /* In each direction,
+       if the neighbour in that direction is an opponent's piece, then:
+           if the checkLine condition is satisfied, then return true. */
     
     return 0;
 }
@@ -250,32 +253,81 @@ char opponent(char player)
 }
 
 
-bool check_line(Piece piece, int dir, char player, Piece &end_of_line)
+bool checkLine(Piece piece, int dir, char player)
 {
-    /* Stores the neighbours of the enemy piece, and the colour of the immediately following 
-       piece in that same direction. */
-    Piece *neighbour[8];
-    piece.get_neighbours(neighbour);
-    char colour = (*neighbour[dir]).get_colour();
+    if (piece.neighbour(dir).colour() == opponent(player))
+	return checkLine(piece.neighbour(dir), dir, player);
+    /* If the neighbour in direction 'dir' is an opponent's piece, then:
+           recursively call checkLine on that neighbour. */
 
-    /* If another enemy piece is detected, this function is recursively called. */
-    if (colour == opponent(player))
-	return check_line(*neighbour[dir], dir, player, end_of_line);
-
-    /* If a friendly piece is detected, the recursion terminates and its address is returned. */
-    if (colour == player) {
-	end_of_line = *neighbour[dir];
+    if (piece.neighbour(dir).colour() == player) {
 	return 1;
+    /* If the neighbour in direction 'dir' is a friendly piece, then:
+           terminate the recursion and return true. */
     }
-    /* Otherwise, the function returns false and the move is not legal. */
+
     return 0;
+    /* If no friendly piece is ever detected, terminate the recursion and return false. */
 }
 
 
-void swap_player(char &player)
+void swapPlayer(char &player)
 {
     if (player == 'W')
 	player = 'B';
     else if (player == 'B')
 	player = 'W';
+}
+
+
+void setBoard(Piece board[8][8], Piece null)
+{   
+    for (int rows=0; rows<8; rows++) {
+	for(int cols=0; cols<8; cols++) {
+	    board[rows][cols].placePiece(' ');
+	    board[rows][cols].setNeighbours(board, rows, cols, null);
+	    board[rows][cols].setLocation(rows, cols);
+	}
+    }
+    /* Initialises the board state to empty and sets each piece's neighbours and coordinates. */
+    
+    board[3][3].placePiece('W');
+    board[3][4].placePiece('B');
+    board[4][3].placePiece('B');
+    board[4][4].placePiece('W');
+    /* Places the beginning four pieces on the board. */
+}
+
+
+void transformBoard(Piece board[8][8], Move move)
+{
+    for (int dir=0; dir<8; dir++) {
+	if ( board[move.row()][move.col()].neighbour(dir).colour() == opponent(move.player()) )
+	    transformLine(board[move.row()][move.col()], dir, move.player(), board);
+    }
+    /* In each direction,
+       if the neighbour in that direction is an opponent's piece, then:
+           flip that line of pieces (only occurs if line is followed by a friendly piece). */
+}
+
+
+bool transformLine(Piece piece, int dir, char player, Piece board[8][8])
+{
+    if (piece.neighbour(dir).colour() == opponent(player)) {
+	if (transformLine(piece.neighbour(dir), dir, player, board)) {
+	    board[piece.row()][piece.col()].placePiece(player);
+	    return 1;
+	}
+    }
+    /* If the piece's neighbour is an opponent's piece, then:
+           recursively call this function, and if it terminates true,
+	   flip all pieces along the way. */
+
+    if (piece.neighbour(dir).colour() == player) {
+	board[piece.row()][piece.col()].placePiece(player);
+	return 1;
+    }
+    /* Only terminates true if a friendly piece is detected at the end of the line. */
+
+    return 0;
 }
